a26313923de3a63c8513a6f589e9550f379c0f94
leopoldavezac <leopoldavezacgmail.com>
2021-09-27 14:32:19 +0200

    feat(alumni_url) - scrape alumni urls

---
+++ networking_bot/alumni_url_scrapping.py
@@ -0,0 +1,47 @@
+from time import sleep
+
+from typing import List, Union
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.common.exceptions import TimeoutException
+
+
+BASE_QUERY_URL = 'https://www.linkedin.com/search/results/people/?origin=FACETED_SEARCH'
+ALUMNI_NM_SPAN_CLASS_NM = 'entity-result__title-line'
+ALUMNI_NM_LINK_CLASS_NM = 'app-aware-link'
+
+def get_alumnis_url(driver: webdriver.Chrome, school_code_str: str, job_title_nm: str, page_index:int) -> List[Union[List[str], bool]]:
+
+    alumnis_url = []
+
+    school_query_url = BASE_QUERY_URL + '&network=%s&schoolFilter=%s&title=%s' % (
+        '%5B"S"%5D',
+        '%5B"'+school_code_str+'"%5D',
+        "%20".join(job_title_nm.split(" "))
+    )
+
+    page_url = school_query_url + '&page=%d' % page_index
+    driver.get(page_url)
+
+    try:
+        alumni_nm_spans = WebDriverWait(driver, 20).until(
+            lambda x: x.find_elements(By.CLASS_NAME, ALUMNI_NM_SPAN_CLASS_NM)
+        )
+    except TimeoutException:
+
+        return [], False
+
+    for alumni_nm_span in alumni_nm_spans:
+
+        a = alumni_nm_span.find_element_by_class_name(ALUMNI_NM_LINK_CLASS_NM)
+        url = a.get_attribute('href')
+        url = url.split('?')[0]
+        alumnis_url.append(url)
+
+    sleep(10)
+
+    return alumnis_url, True
+
+
